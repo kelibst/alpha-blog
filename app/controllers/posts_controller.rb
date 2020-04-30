49265bd6ffdbaacceb5ffa7e6c02dd95cb2e278c
kelibst <kbooster17@gmail.com>
2020-04-30 05:19:41 +0000

    edited the index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,10 +25,10 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-
+    @post.user = current_user
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
